88b55eead6c778c126af0d1f0d21500e80d9bca4
Angell <angell.dev071@gmail.com>
2022-09-01 18:45:14 -0300

    added new lib for module resolver imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-sparse-arrays */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@pages': './src/screens',
+          '@components': '.src/components',
+          test: './test',
+        },
+      },
+    ],
+    ,
+  ],
 };
